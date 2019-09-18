a83ee3ab837b55d4a68422f648165fd96c8b1b69
mshroom <32199029+mshroom@users.noreply.github.com>
2019-09-18 23:13:32 +0300

    Kauppojen lukeminen XML-tiedostosta ja oikeiden kauppojen ID:iden tuominen dropdowniin

--- app/src/main/java/hifian/hintahaukka/Store.java
+++ app/src/main/java/hifian/hintahaukka/Store.java
@@ -1,17 +1,49 @@
 package hifian.hintahaukka;
 
 public class Store {
-    private String googleStoreId;
+    private String storeId;
+    private String name;
+    private double lat;
+    private double lon;
 
-    public Store(String googleStoreId) {
-        googleStoreId = this.googleStoreId;
+    public Store() { }
+
+    public Store(String storeId, String name, double lat, double lon) {
+        this.storeId = storeId;
+        this.name = name;
+        this.lat = lat;
+        this.lon = lon;
     }
 
-    public String getGoogleStoreId() {
-        return this.googleStoreId;
+    public String getName() {
+        return name;
     }
 
-    public void setGoogleStoreId(String googleStoreId) {
-        this.googleStoreId = googleStoreId;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public String getStoreId() {
+        return this.storeId;
+    }
+
+    public void setStoreId(String storeId) {
+        this.storeId = storeId;
     }
 }
