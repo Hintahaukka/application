09924887396996f6385e4aac2a718b123a01847e
MWargelin <matias.wargelin@gmail.com>
2019-09-19 13:35:02 +0300

    Toteuta navigointi NavControllerin avulla

---
+++ app/src/main/java/hifian/hintahaukka/ListPricesFragment.java
@@ -0,0 +1,148 @@
+package hifian.hintahaukka;
+
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListPricesFragment extends Fragment {
+
+    private String ean;
+    private String cents;
+    private String selectedStore;
+    private TextView pricesTextView;
+
+
+    public ListPricesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ListPricesFragmentArgs args = ListPricesFragmentArgs.fromBundle(getArguments());
+        ean = args.getScanResult();
+        selectedStore = args.getSelectedStore();
+        cents = args.getCents();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        pricesTextView = (TextView) getView().findViewById(R.id.pricesTextView);
+        new ListPricesFragment.HerokuPostTask().execute(ean, cents, selectedStore);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_prices, container, false);
+    }
+
+    public class HerokuPostTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String urlString = "https://hintahaukka.herokuapp.com/";
+            String response = "";
+
+            try {
+                URL url = new URL(urlString);
+                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty("Accept-Charset", "UTF-8");
+
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(15000);
+
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("ean", params[0])
+                        .appendQueryParameter("cents", params[1])
+                        .appendQueryParameter("storeId", params[2]);
+                String query = builder.build().getEncodedQuery();
+
+                urlConnection.connect();
+
+                DataOutputStream out = new DataOutputStream(urlConnection.getOutputStream());
+
+                out.writeBytes(query);
+                out.flush();
+                out.close();
+
+
+                int responseCode=urlConnection.getResponseCode();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    String line;
+                    BufferedReader br=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    while ((line=br.readLine()) != null) {
+                        response+=line;
+                    }
+                }
+                urlConnection.disconnect();
+                return response;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return "";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            pricesTextView.setText("Tuotteen "+ ean + " hinnat:\n");
+            try {
+                JSONArray json = new JSONArray(response);
+
+                for (int i = 0; i < json.length(); i++) {
+                    JSONObject priceObject = json.getJSONObject(i);
+                    pricesTextView.append("\nKauppa: " + priceObject.getString("storeId"));
+
+                    double cents = priceObject.getInt("cents") / 100.0;
+                    String formattedPrice = String.format("%.02f", cents);
+                    pricesTextView.append("\nHinta: " + formattedPrice + "€\n");
+                }
+                pricesTextView.setMovementMethod(new ScrollingMovementMethod());
+            } catch (JSONException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+
+}
--- app/src/main/java/hifian/hintahaukka/MainActivity.java
+++ app/src/main/java/hifian/hintahaukka/MainActivity.java
@@ -1,99 +1,35 @@
 package hifian.hintahaukka;
 
-import android.content.Intent;
-import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements BarcodeScannerFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Spinner spinner = (Spinner) findViewById(R.id.storeSpinner);
-        String[] stores = new String[]{
-            String.format(getResources().getString(R.string.text_choose_store)),
-            "1",
-            "2",
-            "3",
-            "4",
-            "5"
-        };
-        final List<String> storeList = new ArrayList<>(Arrays.asList(stores));
-        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
-                this,android.R.layout.simple_spinner_dropdown_item,storeList){
-
-            public boolean isEnabled(int position){
-                if(position == 0)
-                {
-                    // Disable the first item from Spinner
-                    // First item will be use for hint
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-            @Override
-            public View getDropDownView(int position, View convertView,
-                                        ViewGroup parent) {
-                View view = super.getDropDownView(position, convertView, parent);
-                TextView tv = (TextView) view;
-                if(position == 0){
-                    // Set the hint text color gray
-                    tv.setTextColor(Color.GRAY);
-                }
-                else {
-                    tv.setTextColor(Color.BLACK);
-                }
-                return view;
-            }
-        };
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(spinnerArrayAdapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItemText = (String) parent.getItemAtPosition(position);
-                // If user change the default selection
-                // First item is disable and it is used for hint
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-
-
-        Button scanButton = findViewById(R.id.button_scan_barcode);
-        scanButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, BarcodeScannerActivity.class);
-                intent.putExtra("selectedStore", String.valueOf(spinner.getSelectedItem()));
-                startActivity(intent);
-            }
-        });
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController);
     }
 
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp();
+    }
+
+
+    @Override
+    public void onFragmentInteraction() {
+
+    }
 }
