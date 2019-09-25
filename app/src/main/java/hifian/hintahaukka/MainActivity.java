7a479116d3589662d08044c4534d567121b0267a
mmohamud <mohamed.a.mohamud@helsinki.fi>
2019-09-25 18:38:58 +0300

    Gps paikannus lisätty

--- app/src/main/java/hifian/hintahaukka/MainActivity.java
+++ app/src/main/java/hifian/hintahaukka/MainActivity.java
@@ -1,8 +1,12 @@
 package hifian.hintahaukka;
 
+import android.Manifest;
+import android.location.Location;
 import android.os.Bundle;
-
+import android.widget.Toast;
+import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -10,10 +14,18 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GpsActivity gpsActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.gpsActivity = new GpsActivity(getApplicationContext());
+        try {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController);
@@ -22,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp();
+    }
+
+    public GpsActivity getGpsActivity() {
+        return this.gpsActivity;
     }
 
 }
