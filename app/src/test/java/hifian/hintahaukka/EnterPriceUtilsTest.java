5be867a1d4edc965c754bb816ab40cf9cdd0b529
MWargelin <matias.wargelin@gmail.com>
2019-09-25 12:44:39 +0300

    Lisää testit hinnan lisäys -ominaisuudelle

---
+++ app/src/test/java/hifian/hintahaukka/EnterPriceUtilsTest.java
@@ -0,0 +1,24 @@
+package hifian.hintahaukka;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EnterPriceUtilsTest {
+
+    @Test
+    public void turnEnteredPriceToCentsWorksCorrectly() {
+        String euros = "35";
+        String cents = "05";
+
+        String result = EnterPriceUtils.turnEnteredPriceToCents(euros, cents);
+
+        assertEquals("3505", result);
+    }
+
+    @Test
+    public void givingEmptyStringsToTurnEnteredPriceToCentsReturnsZero() {
+        String result = EnterPriceUtils.turnEnteredPriceToCents("", "");
+        assertEquals("0", result);
+    }
+}
