f9fbcf41650bf243b9539e35bb7e9a1bfd255579
MWargelin <matias.wargelin@gmail.com>
2019-12-05 11:35:22 +0200

    Refaktoroi käyttäjän pisteiden päivitys UI:hin
    Siirtää käyttäjien pisteiden piirtämisen käyttöliittymään pois
    StoreListFragmentista loogisempaan paikkaan UserManagerin vastuulle.

--- app/src/main/java/hifian/hintahaukka/GUI/ShoppingCartFragment.java
+++ app/src/main/java/hifian/hintahaukka/GUI/ShoppingCartFragment.java
@@ -10,7 +10,6 @@ import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
